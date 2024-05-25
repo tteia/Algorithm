d7e7d66e229d61c75915c173deecf4a8b462ce79
tteia <tteia.dl@gmail.com>
2024-05-25 09:51:39 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 300 ms, Memory: 18132 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,16 @@
+import java.util.IllegalFormatCodePointException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        for(int i = 0; i < A; i++){
+            for(int j = 0; j < i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
