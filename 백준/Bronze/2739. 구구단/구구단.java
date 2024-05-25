921483bca0314fc94826350b7a7b579c6d7654ed
tteia <tteia.dl@gmail.com>
2024-05-25 09:29:01 +0900

    [Bronze V] Title: 구구단, Time: 232 ms, Memory: 18552 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,12 @@
+import java.util.IllegalFormatCodePointException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(A + " * " + i + " = " + A * i);
+        }
+    }
+}
