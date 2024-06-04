f84cbd2859ffda076d59414a59691238dfcf50de
tteia <tteia.dl@gmail.com>
2024-06-04 23:09:21 +0900

    [Gold IV] Title: 부분합, Time: 876 ms, Memory: 102544 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,40 +1,36 @@
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int S = sc.nextInt();
         int sum = 0;
-        int[] nums = new int[N+1];
+        int[] nums = new int[N];
         int min = Integer.MAX_VALUE;
         int start = 0;
         int end = 0;
 
-
         for (int i = 0; i < N; i++){
             nums[i] = sc.nextInt();
         }
-        // 시작점과 끝점은 배열 안에 있어야 하니까!
-        while(start <= N && end <= N){
-            // 구간의 합이 S 보다 큰 경우를 찾는다 ! -> sum 이 S 보다 크거나,
-            // start ~ end 까지의 길이가 end - start 보다 클 때.
-            if(sum >= S && min > end - start){
-                min = end - start;
-            }
 
-            if(sum < S){
-                sum += nums[end++];
-            }
-            else {
+        while (end <= N) {
+            if (sum >= S) {
+                min = Math.min(min, end - start);
                 sum -= nums[start++];
+            } else {
+                if (end < N) {
+                    sum += nums[end++];
+                } else {
+                    break; // end가 N 이상으로 증가하는 것을 방지하기 위해 루프 종료
+                }
             }
         }
+        
         if (min == Integer.MAX_VALUE){
             System.out.println("0");
-        }
-        else{
+        } else {
             System.out.println(min);
         }
     }
