ac55d95ccfce1819eca05ade0924ef6a0f7186d4
tteia <tteia.dl@gmail.com>
2024-06-05 12:34:47 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int R1, S;
+    cin >> R1 >> S;
+
+    cout << (2 * S) - R1;
+}
