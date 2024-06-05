b95f4f75cf2aa185d7522a202b2bf90bd3d5c416
tteia <tteia.dl@gmail.com>
2024-06-05 15:02:24 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 852 ms, Memory: 221536 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sb.append(A + B).append("\n");
+        }
+        
+        System.out.print(sb.toString());
+    }
+}
