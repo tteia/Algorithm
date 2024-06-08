bda7a66ca5b4246cbbe0193e5175631549fb5043
tteia <tteia.dl@gmail.com>
2024-06-08 16:02:02 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 200 ms, Memory: 17516 KB -BaekjoonHub

---
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] kda = sc.nextLine().split("/");
+        int k = Integer.parseInt(kda[0]);
+        int d = Integer.parseInt(kda[1]);
+        int a = Integer.parseInt(kda[2]);
+
+        if(k+a < d || d == 0){
+            System.out.println("hasu");
+        }
+        else{
+            System.out.println("gosu");
+        }
+    }
+}
