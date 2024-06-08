513cffb645945da7a18ed3326bf9bc651c52cb5a
tteia <tteia.dl@gmail.com>
2024-06-08 16:15:19 +0900

    [Bronze IV] Title: 입실 관리, Time: 232 ms, Memory: 17844 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        sc.nextLine();
+        
+        for(int i = 0; i < N; i++){
+            String name = sc.nextLine();
+            String answer = name.toLowerCase();
+            System.out.println(answer);
+        }
+    }
+}
