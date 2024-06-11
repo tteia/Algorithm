e2e5a068fdc71aba7ada7ef5c5386ed15506cf89
tteia <tteia.dl@gmail.com>
2024-06-12 00:09:42 +0900

    [level 2] Title: 더 맵게, Time: 1658.40 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,27 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        Queue<Integer> tmp = new PriorityQueue<>();
-        for(int s : scoville){
-            tmp.add(s);
-        }
-        if(tmp.peek() >= K){
-            return 0;
-        }
-        
-        int cnt = 0;
-        while(tmp.peek() < K && tmp.size() > 1){
-            int first = tmp.poll();
-            int second = tmp.poll();
-            tmp.add(first+(second * 2));
-            cnt++;
-        }
-        if(tmp.peek() < K){
-            answer = -1;
-        }
-        else answer = cnt;
         return answer;
     }
 }
