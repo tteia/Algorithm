be94ec5e71354d76094c79fba8c6fc2918903170
tteia <tteia.dl@gmail.com>
2024-06-12 13:59:26 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 91.01 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,14 +2,23 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        Stack<Integer> tmp = new Stack<>();
-        
-        for (int i : arr) {
-            if (tmp.isEmpty() || tmp.peek() != i) {
-                tmp.push(i);
+
+        Stack<Integer> tmp = new Stack<Integer>();
+        for(int i : arr){
+            tmp.push(i);
+            if(tmp.size() > 1){                
+                int before = tmp.pop();         
+                int after = tmp.pop();          
+                if(before == after){            
+                    tmp.push(after);           
+                }
+                else{                           
+                    tmp.push(after);           
+                    tmp.push(before);
+                }
             }
         }
-        
+
         int[] answer = new int[tmp.size()];
         
         for (int i = answer.length - 1; i >= 0; i--) {
