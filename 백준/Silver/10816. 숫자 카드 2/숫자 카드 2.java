2585dd2459787e9355b90670caa8d623b78c76b4
tteia <tteia.dl@gmail.com>
2024-11-27 00:36:07 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1608 ms, Memory: 184096 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        // 가진 카드 배열로 만들기
+        int[] cards = new int[N];
+        String nums = br.readLine();
+        StringTokenizer tmp = new StringTokenizer(nums);
+        int index = 0;
+        while (tmp.hasMoreTokens()) {
+            cards[index] = Integer.parseInt(tmp.nextToken());
+            index++;
+        }
+        Arrays.sort(cards);
+
+        // 각 숫자에 대한 등장 횟수를 저장할 Map
+        Map<Integer, Integer> cardCount = new HashMap<>();
+        for (int card : cards) {
+            cardCount.put(card, cardCount.getOrDefault(card, 0) + 1);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+
+        // 각 숫자에 대해 등장 횟수 계산
+        String finds = br.readLine();
+        StringTokenizer hm = new StringTokenizer(finds);
+        int idx = 0;
+        while (hm.hasMoreTokens()) {
+            int num = Integer.parseInt(hm.nextToken());
+            int cnt = cardCount.getOrDefault(num, 0);
+            bw.write(cnt + " ");
+            idx++;
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
