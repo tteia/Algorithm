efc09521bbd48622dbbb847ae03f18a6b9b5803e
tteia <tteia.dl@gmail.com>
2025-02-21 17:34:10 +0900

    [Silver V] Title: 날짜 계산, Time: 116 ms, Memory: 15872 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] input = br.readLine().split(" ");
+        int E = Integer.parseInt(input[0]);
+        int S = Integer.parseInt(input[1]);
+        int M = Integer.parseInt(input[2]);
+        
+        int year = 1;
+        while (true) {
+            if ((year - E) % 15 == 0 && (year - S) % 28 == 0 && (year - M) % 19 == 0) {
+                bw.write(year + "\n");
+                break;
+            }
+            year++;
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
