4d115940954fd56812fd60e57e145917c7756ad2
tteia <tteia.dl@gmail.com>
2025-03-10 15:11:44 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 11.83 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        String[] arr = s.split(" ");
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < arr.length; i++){
+            int num = Integer.parseInt(arr[i]);
+            if(num < min) min = num;
+            if(num > max) max = num;
+        }
+        
+        
+        return min + " " + max;
+    }
+}
