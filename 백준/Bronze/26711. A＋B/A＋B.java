c8f5fd59e9235d8ecd5de2874cef9dfbd263fc80
tteia <tteia.dl@gmail.com>
2025-04-02 16:19:48 +0900

    [Bronze V] Title: A+B, Time: 288 ms, Memory: 18640 KB -BaekjoonHub

---
+++ 백준/Bronze/26711. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = new BigInteger(sc.nextLine());
+        BigInteger b = new BigInteger(sc.nextLine());
+        System.out.println(a.add(b));
+    }
+}
