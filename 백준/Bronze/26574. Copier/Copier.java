40972c1365ee61a719aefda7796f07813ca40592
Ayeong Choi <tteia.dl@gmail.com>
2025-04-13 12:08:42 +0900

    [Bronze V] Title: Copier, Time: 180 ms, Memory: 18384 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        for(int i = 0; i < N; i++){
+            int a = sc.nextInt();
+            System.out.println(a + " " + a);
+        }
+    }
+}
