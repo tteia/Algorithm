c4968c7e0c35b84dfb0c174d512bdc7a5ff86344
Ayeong Choi <tteia.dl@gmail.com>
2025-04-20 13:25:10 +0900

    [Bronze V] Title: ブラックジャック (Blackjack), Time: 172 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/33170. ブラックジャック （Blackjack）/ブラックジャック （Blackjack）.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        if (A + B + C <= 21) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
