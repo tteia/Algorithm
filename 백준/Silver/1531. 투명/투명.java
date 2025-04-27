c29dd5185bb609c99461498101af2362c6559813
Ayeong Choi <tteia.dl@gmail.com>
2025-04-27 22:34:56 +0900

    [Silver V] Title: 투명, Time: 208 ms, Memory: 18100 KB -BaekjoonHub

---
+++ 백준/Silver/1531. 투명/투명.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[][] grid = new int[101][101];
+
+        for (int i = 0; i < N; i++) {
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+
+            for (int x = x1; x <= x2; x++) {
+                for (int y = y1; y <= y2; y++) {
+                    grid[x][y]++;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int x = 1; x <= 100; x++) {
+            for (int y = 1; y <= 100; y++) {
+                if (grid[x][y] > M) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
