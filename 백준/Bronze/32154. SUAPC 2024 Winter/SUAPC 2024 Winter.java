a5fd483fcab7bb19af9efae93ed9b5607fa5ba0c
Ayeong Choi <tteia.dl@gmail.com>
2025-05-05 13:05:56 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 96 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String[][] board = {{"11", "A B C D E F G H J L M"},
+                {"9", "A C E F G H I L M"},
+                {"9", "A C E F G H I L M"},
+                {"9", "A B C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A B C F G H L M"}};
+        
+        System.out.println(board[N-1][0]);
+        System.out.println(board[N-1][1]);
+    }
+}
