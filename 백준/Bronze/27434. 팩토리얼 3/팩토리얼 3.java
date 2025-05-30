aff7e3515fad3f02a8425abee0c2b254b9f29e7e
Ayeong Choi <tteia.dl@gmail.com>
2025-05-30 11:45:54 +0900

    [Bronze V] Title: 팩토리얼 3, Time: 3088 ms, Memory: 402076 KB -BaekjoonHub

---
+++ 백준/Bronze/27434. 팩토리얼 3/팩토리얼 3.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.valueOf(br.readLine());
+
+        System.out.println(fact(1,N));
+    }
+    public static BigInteger fact(int a,int n){
+        BigInteger ret=BigInteger.valueOf(a);
+
+        if(a<n){
+            int b=(a+n)/2;
+            ret=fact(a,b).multiply(fact(b+1,n)); //두개로 나눠서 풀이
+        }
+        return ret;
+    }
+}
