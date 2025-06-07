72733234125318763802ea1abddc59d8ea504cc9
Ayeong Choi <tteia.dl@gmail.com>
2025-06-07 11:09:43 +0900

    [level 2] Title: 피로도, Time: 5.58 ms, Memory: 92.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    int answer = 0;
+    boolean[] visited;
+
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(k, dungeons, 0);
+        return answer;
+    }
+
+    void dfs(int k, int[][] dungeons, int count) {
+        answer = Math.max(answer, count);
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(k - dungeons[i][1], dungeons, count + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
