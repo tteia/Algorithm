da7117b949a749e9997cd54ca66ff64c12c4c51d
Ayeong Choi <tteia.dl@gmail.com>
2025-08-19 23:31:14 +0900

    [level 2] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/README.md
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 조건에 맞는 도서와 저자 리스트 출력하기 - 144854 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144854) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144854?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 05월 21일 09:44:33
+2025년 08월 19일 23:31:11
 
 ### 문제 설명
 
--- 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -1,1 +1,7 @@
-SELECT b.BOOK_ID, a.AUTHOR_NAME, DATE_FORMAT(b.PUBLISHED_DATE, '%Y-%m-%d') as PUBLISHED_DATE FROM BOOK b INNER JOIN AUTHOR a ON b.author_id = a.author_id WHERE b.CATEGORY = '경제' ORDER BY b.PUBLISHED_DATE;
+SELECT     b.book_id
+         , a.author_name
+         , TO_CHAR(b.published_date, 'YYYY-MM-DD') AS published_date
+FROM       book b
+INNER JOIN author a ON b.author_id = a.author_id
+WHERE      b.category = '경제'
+ORDER BY   b.published_date ASC;
