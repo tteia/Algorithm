71f2daa871e747d991c7de91919289591e404fa3
Ayeong Choi <tteia.dl@gmail.com>
2025-08-27 20:57:58 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,7 +1,8 @@
 import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -9,27 +10,29 @@ public class Main {
         int cnt = 0;
 
         for(int i = 0; i < N; i++){
-            String line = br.readLine();
-            boolean groupCheck = true;
-            boolean[] visited = new boolean[26];
 
-            for(int j = 0; j < line.length(); j++) {
-                int currentChar = line.charAt(j) - 'a';
-                if(j > 0 && line.charAt(j) != line.charAt(j - 1) && visited[currentChar]) {
-                    groupCheck = false;
+            String word = br.readLine();
+            Map<Character, Boolean> map = new HashMap<>();
+            boolean isGroup = true;
+            char prev = 0;
+
+            for (int j = 0; j < word.length(); j++) {
+                char c = word.charAt(j);
+                if (map.containsKey(c) && prev != c) {
+                    // 이미 나온 문자이고, 바로 앞 문자랑 다르면 그룹 단어 아님
+                    isGroup = false;
                     break;
                 }
-                visited[currentChar] = true;
+                map.put(c, true); // 등장 기록
+                prev = c;
             }
 
-            if(groupCheck) {
-                cnt++;
-            }
+            if (isGroup) cnt++;
         }
 
         bw.write(String.valueOf(cnt));
         bw.flush();
         bw.close();
-        br.close();
+        
     }
 }
