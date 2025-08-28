6a39287003401a2ea8fdc9623af7e5088fff8f81
Ayeong Choi <tteia.dl@gmail.com>
2025-08-28 21:08:06 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,9 +1,7 @@
 import java.io.*;
-import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -11,26 +9,67 @@ public class Main {
         int cnt = 0;
 
         for(int i = 0; i < line.length(); i++){
-            cnt++;
-            if(line.charAt(i) == '=' && i >= 1){
-                if(line.charAt(i - 1) == 'c' || line.charAt(i - 1) == 's'){
-                    cnt--;
-                } else if (line.charAt(i - 1) == 'z'){
-                    if(i >= 2 && line.charAt(i - 2) == 'd'){
-                        cnt--;
-                        cnt--;
-                    } else if ((i >= 2 && line.charAt(i - 2) != 'd') || i >= 1 && line.charAt(i - 1) == 'z') {
-                        cnt--;
+            switch (line.charAt(i)){
+                case 'c':
+                    if(i + 1 < line.length() && (line.charAt(i + 1) == '=' || line.charAt(i + 1) == '-')){
+                        cnt++;
+                        i++; // 다음 문자도 건너뛰기
+                    } else {
+                        cnt++;
                     }
-                }
-            } else if (line.charAt(i) == '-' && i >= 1) {
-                if(line.charAt(i - 1) == 'c' || line.charAt(i - 1) == 'd'){
-                    cnt--;
-                }
-            } else if (line.charAt(i) == 'j' && i >= 1) {
-                if(line.charAt(i - 1) == 'l' || line.charAt(i - 1) == 'n'){
-                    cnt --;
-                }
+                    break;
+                
+                case 'd':
+                    if(i + 2 < line.length() && line.charAt(i + 1) == 'z' && line.charAt(i + 2) == '='){
+                        cnt++;
+                        i += 2; // dz= 전체를 건너뛰기
+                    } else if(i + 1 < line.length() && line.charAt(i + 1) == '-'){
+                        cnt++;
+                        i++; // d- 건너뛰기
+                    } else {
+                        cnt++;
+                    }
+                    break;
+                
+                case 'l':
+                    if(i + 1 < line.length() && line.charAt(i + 1) == 'j'){
+                        cnt++;
+                        i++; // lj 건너뛰기
+                    } else {
+                        cnt++;
+                    }
+                    break;
+                
+                case 'n':
+                    if(i + 1 < line.length() && line.charAt(i + 1) == 'j'){
+                        cnt++;
+                        i++; // nj 건너뛰기
+                    } else {
+                        cnt++;
+                    }
+                    break;
+                
+                case 's':
+                    if(i + 1 < line.length() && line.charAt(i + 1) == '='){
+                        cnt++;
+                        i++; // s= 건너뛰기
+                    } else {
+                        cnt++;
+                    }
+                    break;
+                
+                case 'z':
+                    if(i + 1 < line.length() && line.charAt(i + 1) == '='){
+                        cnt++;
+                        i++; // z= 건너뛰기
+                    } else {
+                        cnt++;
+                    }
+                    break;
+                
+                default:
+                    cnt++;
+                    break;
             }
         }
 
