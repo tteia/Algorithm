1e690a983c7ca533310057d563abfabac1fa1236
Ayeong Choi <tteia.dl@gmail.com>
2025-08-31 19:36:32 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.40 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,16 +1,23 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s) {
-        String[] arr = s.split(" ");
-        int min = Integer.MAX_VALUE;
+        // s 에는 공백으로 구분된 숫자들이 저장되어 있음
+        // 최소값과 절대값을 찾아 문자열을 반환하는 함수
+        StringTokenizer st = new StringTokenizer(s);
         int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
         
-        for(int i = 0; i < arr.length; i++){
-            int num = Integer.parseInt(arr[i]);
-            if(num < min) min = num;
+        while(st.hasMoreTokens()){
+            int num = Integer.parseInt(st.nextToken());
             if(num > max) max = num;
+            if(num < min) min = num;
         }
+        StringBuilder sb = new StringBuilder();
+        sb.append(min);
+        sb.append(" ");
+        sb.append(max);
         
-        
-        return min + " " + max;
+        return String.valueOf(sb);
     }
 }
