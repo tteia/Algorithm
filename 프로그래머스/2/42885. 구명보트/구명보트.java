be992d1c960330fad028a30c46cec36233ea2414
Ayeong Choi <tteia.dl@gmail.com>
2025-09-08 23:25:04 +0900

    [level 2] Title: 구명보트, Time: 16.29 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,21 +1,33 @@
 import java.util.*;
 
 class Solution {
+
     public int solution(int[] people, int limit) {
-        Arrays.sort(people);
-        
-        int left = 0;
-        int right = people.length - 1;
-        int cnt = 0;
-        
-        while (left <= right) {
-            if (people[left] + people[right] <= limit) {
-                left++;
-            }
-            right--;
-            cnt++;
+        int count = 0;
+        int[] weights = new int[241]; // 몸무게 카운팅 배열 (40~240)
+
+        // 각 몸무게별 인원 수 카운트
+        for (int w : people) {
+            weights[w]++;
         }
-        
-        return cnt;
+
+        // 무거운 사람부터 처리
+        for (int i = 240; i >= 1; i--) {
+            while (weights[i] > 0) {
+                weights[i]--; // 가장 무거운 사람 태움
+
+                // 같이 탈 수 있는 가장 가벼운 사람 탐색
+                for (int j = 1; j <= limit - i; j++) {
+                    if (weights[j] > 0) {
+                        weights[j]--;
+                        break;
+                    }
+                }
+
+                count++; // 보트 하나 사용
+            }
+        }
+
+        return count;
     }
 }
