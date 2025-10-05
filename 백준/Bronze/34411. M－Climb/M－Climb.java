4d7adb777bf92171843ee85db7ba0fa124cc0c7b
Ayeong Choi <tteia.dl@gmail.com>
2025-10-05 14:08:59 +0900

    [Bronze V] Title: M-Climb, Time: 176 ms, Memory: 17504 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(m * n);
+    }
+}
