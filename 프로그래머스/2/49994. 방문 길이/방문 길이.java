23cc7c87112cb5d91a2a278a80960aa42133280b
Ayeong Choi <tteia.dl@gmail.com>
2025-10-31 19:42:55 +0900

    [level 2] Title: 방문 길이, Time: 17.82 ms, Memory: 97.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String dirs) {
+        Set<String> set = new HashSet<>();
+        int x = 0, y = 0;
+        for (char c : dirs.toCharArray()) {
+            int nx = x, ny = y;
+            if (c == 'U') ny++;
+            else if (c == 'D') ny--;
+            else if (c == 'R') nx++;
+            else if (c == 'L') nx--;
+            if (nx < -5 || nx > 5 || ny < -5 || ny > 5) continue;
+            String path1 = x + "," + y + ":" + nx + "," + ny;
+            String path2 = nx + "," + ny + ":" + x + "," + y;
+            set.add(path1);
+            set.add(path2);
+            x = nx;
+            y = ny;
+        }
+        return set.size() / 2;
+    }
+}
