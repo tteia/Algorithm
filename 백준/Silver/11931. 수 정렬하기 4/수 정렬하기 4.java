ddd2520ec438378e0aa1fc151706187aba68036c
Ayeong Choi <tteia.dl@gmail.com>
2025-11-18 17:17:23 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1540 ms, Memory: 161956 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < N; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+
+		Collections.sort(list,Collections.reverseOrder());
+
+		for (int i : list) {
+			sb.append(i + "\n");
+		}
+		System.out.println(sb);
+
+	}
+}
