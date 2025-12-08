d47166aa7292cc3b7949d309531edfe505c2d278
Ayeong Choi <tteia.dl@gmail.com>
2025-12-08 19:59:37 +0900

    [Bronze V] Title: Shares, Time: 176 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/3733. Shares/Shares.java
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -1,15 +1,13 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        while(sc.hasNextInt()){
-            int N = sc.nextInt();
-            long S = sc.nextLong();
-            long x = S / (N + 1);
-            System.out.println(x);
-            
+        int n, s;
+        while(sc.hasNext()){
+            n = sc.nextInt();
+            s = sc.nextInt();
+            System.out.println(s / (n + 1));
         }
     }
 }
