d8e7a1a01c1007de4a0fea1110c5b041d2e1d3a2
Ayeong Choi <tteia.dl@gmail.com>
2025-12-08 19:58:39 +0900

    [Silver IV] Title: 제로, Time: 200 ms, Memory: 23336 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,23 +1,32 @@
-import java.util.Scanner;
-import java.util.Stack;
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int K = sc.nextInt();
-        int sum = 0;
-        Stack<Integer> stack = new Stack<>();
-        for(int i = 0; i < K; i++){
-            int n = sc.nextInt();
-            if(n == 0){
-                stack.pop();
-            }
-            else stack.add(n);
-        }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (Integer i : stack) {
-            sum += i;
-        }
-        System.out.println(sum);
-    }
+		int K = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<>();
+
+		for(int i = 0; i < K; i++) {
+			int temp = Integer.parseInt(br.readLine());
+
+			if(temp == 0) {
+				stack.pop();
+			}
+			else {
+				stack.push(temp);
+			}
+
+		}
+
+		int sum = 0;
+		for(int num : stack) {
+			sum += num;
+		}
+
+		System.out.println(sum);
+
+	}
 }
