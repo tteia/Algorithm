81bbdd148dcb7034a773b0b492206bf441e7afcb
Ayeong Choi <tteia.dl@gmail.com>
2025-12-12 15:03:40 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 124 ms, Memory: 14844 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -1,36 +1,34 @@
 import java.io.*;
 
 public class Main {
-    static public StringBuilder sb = new StringBuilder();
+    static int cnt = 0;
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+        System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
 
-        int N = Integer.parseInt(br.readLine());
-
-        recursive(N, "");
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    public static void recursive(int N, String blank) {
-        sb.append(blank).append("\"재귀함수가 뭔가요?\"\n");
-
-        if (N == 0) {
-            sb.append(blank).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
-        } else {
-            sb.append(blank).append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
-            sb.append(blank).append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
-            sb.append(blank).append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
-            recursive(N - 1, blank + "____");
+        for(int i = 0; i <= n; i++){
+            System.out.println(bar() + "\"재귀함수가 뭔가요?\"");
+            if(i == n){
+                System.out.println(bar() + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+            } else {
+                System.out.println(bar() + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+                System.out.println(bar() + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+                System.out.println(bar() + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+            }
+            cnt++;
         }
 
-        sb.append(blank).append("라고 답변하였지.\n");
+        cnt--;
+        for(int i = 0; i <= n; i++){
+            System.out.println(bar() + "라고 답변하였지.");
+            cnt--;
+        }
+    }
+
+    static String bar() {
+        return "____".repeat(cnt);
     }
 }
