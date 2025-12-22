f06a14d94bf3c67d3a3fd46c4267fbbd3c95039f
Ayeong Choi <tteia.dl@gmail.com>
2025-12-22 23:12:01 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 45508 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -3,25 +3,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        Queue<Integer> q = new LinkedList<>();
         int N = Integer.parseInt(br.readLine());
-
-        for(int i = 1; i <= N; i++) {
-            q.offer(i);
-        }
-
-        while(q.size() > 1) {
-            q.poll();
-            q.offer(q.poll());
-        }
+        Queue<Integer> queue = new LinkedList<>();
         
-        bw.write(String.valueOf(q.poll()));
-        bw.flush();
-        bw.close();
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+        while(queue.size() > 1){
+            if(queue.size() == 1){
+                break;
+            }
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.peek());
         br.close();
     }
 }
