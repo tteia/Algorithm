400b26746af21e25ac3b4457fcf1d44d3397e700
Ayeong Choi <tteia.dl@gmail.com>
2025-12-31 21:04:39 +0900

    [Bronze V] Title: Rook, Time: 96 ms, Memory: 13912 KB -BaekjoonHub

--- 백준/Bronze/2393. Rook/Rook.java
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -1,26 +1,18 @@
-import java.io.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        bw.write("  ___  ___  ___\n");
-        bw.write("  | |__| |__| |\n");
-        bw.write("  |           |\n");
-        bw.write("   \\_________/\n");
-        bw.write("    \\_______/\n");
-        bw.write("     |     |\n");
-        bw.write("     |     |\n");
-        bw.write("     |     |\n");
-        bw.write("     |     |\n");
-        bw.write("     |_____| \n");
-        bw.write("  __/       \\__\n");
-        bw.write(" /             \\\n");
-        bw.write("/_______________\\\n");
-
-        bw.flush();
-        bw.close();
-        br.close();
+    
+    public static void main(String[] args) {
+        System.out.println("  ___  ___  ___");
+        System.out.println("  | |__| |__| |");
+        System.out.println("  |           |");
+        System.out.println("   \\_________/");
+        System.out.println("    \\_______/");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |_____|");
+        System.out.println("  __/       \\__");
+        System.out.println(" /             \\");
+        System.out.println("/_______________\\");
     }
 }
