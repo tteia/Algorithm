c843624d08bbb6ad044be308198e451ca0776d16
Ayeong Choi <tteia.dl@gmail.com>
2026-01-03 23:18:23 +0900

    [Silver II] Title: 스택 수열, Time: 304 ms, Memory: 26804 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -2,40 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
-        Stack<Integer> stack = new Stack<>();
-        N = Integer.parseInt(br.readLine());
-        int start = 0;
-        for(int i = 0; i < N; i++){
-            int num = Integer.parseInt(br.readLine());
-            if(num > start){
-                for(int j = start + 1; j <= num; j++) {
-                    stack.push(j);
-                    sb.append("+").append("\n");
-                }
-                start = num;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int C = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> st = new Stack<>();
+
+        int current = 1;
+        for (int i = 0; i < C; i++) {
+            int N = Integer.parseInt(br.readLine());
+
+            while (current <= N) {
+                st.push(current++);
+                sb.append("+\n");
             }
-            else if(stack.peek() != num) {
-                bw.write("NO");
-                bw.flush();
-                bw.close();
-                br.close();
+
+            if (!st.isEmpty() && st.peek() == N) {
+                st.pop();
+                sb.append("-\n");
+            } else {
+                System.out.println("NO");
                 return;
             }
-            stack.pop();
-            sb.append("-").append("\n");
         }
 
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.print(sb.toString());
     }
 }
