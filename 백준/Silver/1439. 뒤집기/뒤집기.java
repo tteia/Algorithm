1c2386b07cf9b5bae6e6db45f9224c7b99f11ec9
Ayeong Choi <tteia.dl@gmail.com>
2026-01-12 23:42:42 +0900

    [Silver V] Title: 뒤집기, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,19 +1,28 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String line = br.readLine();
-        StringTokenizer s0 = new StringTokenizer(line, "0");
-        StringTokenizer s1 = new StringTokenizer(line, "1");
-
-        bw.write(String.valueOf(Math.min(s0.countTokens(), s1.countTokens())));
-        bw.flush();
-        bw.close();
-        br.close();
+        String s = br.readLine();
+        int zeroCount = 0;
+        int oneCount = 0;
+        
+        if (s.charAt(0) == '0') {
+            zeroCount++;
+        } else {
+            oneCount++;
+        }
+        
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(i - 1)) {
+                if (s.charAt(i) == '0') {
+                    zeroCount++;
+                } else {
+                    oneCount++;
+                }
+            }
+        }
+        
+        System.out.println(Math.min(zeroCount, oneCount));
     }
 }
