25762151174bcd5ef4f867fe3facc473a3fa1ba1
Ayeong Choi <tteia.dl@gmail.com>
2026-01-17 22:10:33 +0900

    [Silver V] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.java
+++ 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.java
@@ -1,27 +1,32 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        boolean[][] grid = new boolean[101][101];
-        int area = 0;
-
-        for (int i = 0; i < 4; i++) {
-            int x1 = sc.nextInt();
-            int y1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            int y2 = sc.nextInt();
-
-            for (int x = x1; x < x2; x++) {
-                for (int y = y1; y < y2; y++) {
-                    if (!grid[x][y]) {
-                        grid[x][y] = true;
-                        area++;
-                    }
-                }
-            }
-        }
-
-        System.out.println(area);
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int [][] xy = new int[101][101];
+		int answer = 0;
+		for (int i = 0; i < 4; i++) {
+			int x1 = sc.nextInt();
+			int y1 = sc.nextInt();
+			int x2 = sc.nextInt();
+			int y2 = sc.nextInt();
+			
+			for(int xx = x1; xx < x2; xx++) {
+				for (int yy = y1; yy < y2; yy++ ) {
+					xy[xx][yy] = 1;
+				}
+			}
+		}
+		
+		
+		for (int i = 0; i < 101; i++) {
+			for(int u = 0; u < 101; u++) {
+				if (xy[i][u] == 1) {
+					answer ++;
+				}
+			}
+		}
+		
+		System.out.println(answer);
+	}
 }
